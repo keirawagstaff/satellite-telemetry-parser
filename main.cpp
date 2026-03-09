d4b7dbee0b874367a52010360153e600bd980684
Keira Wagstaff <keirawagstaff@gmail.com>
2026-03-09 11:53:23 -0600

    skeleton for int main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
+#include <iostream>
 #include <fstream>
 #include <sstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int main(){
-    
+    string inputFile = "telemetry_data.csv";
+    string logFile = "telemetry_log.csv";
+
+    ifstream inFile(inputFile);
+    ofstream outLog(logFile);
+
+    if(!inFile.is_open()){
+        cout << "Could not open input file" << endl;
+    }
+
+    string line;
+    getline(inFile, line);
+    outLog << line << endl;
+
+    while(getline(inFile, line)){
+        if(line.empty())continue;
+
+        outLog << line << endl;
+    }
+
+    return 0;
 }
